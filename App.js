f3675ebe2633183d7723a7ec5bc9e7297eea5060
Ifra Idrees <ifraidrees.ngxoft@gmail.com>
2020-12-16 13:46:10 +0500

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {useState} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View, Image, StyleSheet,Text, TextInput } from 'react-native';
+
+
+const  Output  =(props)=> {
+    return (
+      <View style={styles.container}>
+  
+           <Text  style={{fontSize:20}}>
+             {props.data}     
+        </Text>
+                
+      </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Input  = () =>{
+
+    const [getText,setText]=useState('');
+
+    return (
+      <View style={styles.container}>
+
+        <TextInput  
+        style={styles.inputbg}
+        placeholder='Enter text here'
+        onChangeText={ text =>setText(text)}
+        />
+
+        <Output  data={getText}/>
+
+      </View>
+  );
+
+ 
+
+ 
+  
+ 
+  
+}
+
+class DisplayInputOutput extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+          <Input/>
+          <Output/>
+      </View>
+    );
+  }
+}
+
+
+  const styles = StyleSheet.create({
+    container: {
+
+      flexDirection:'column',
+      justifyContent:'center',
+      alignItems:'center',
+      marginTop:50
+    },
+    tinyLogo: {
+      width: 100,
+      height: 100,
+     },
+     textbg:{
+       fontSize:20,
+       marginTop:30,
+       
+     },
+     inputbg:{
+       borderWidth:1,
+       borderColor:'#777',
+       padding:8,
+       margin:10,
+       width:200,
+     }
+   
+  });
+
+export default DisplayInputOutput;
